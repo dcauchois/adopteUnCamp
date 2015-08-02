bbc4c5875da464bf78188a50b3d69f5ea2458fbf
Denis <Denis@Edsger>
2015-08-02 18:30:14 +0200

    Mise en place de spring

---
+++ src/main/java/com/dcs/adopte1camp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dcs.adopte1camp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController 
+{
+	   @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot!";
+	    }
+}
